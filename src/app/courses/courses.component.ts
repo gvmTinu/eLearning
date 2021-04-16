58f2830e0e8bf7e9fb6bbe027ed493a237d78483
tinu <gr0psy17@gmail.com>
2021-04-17 00:48:31 +0300

    final release

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -5,11 +5,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.css']
 })
-export class CoursesComponent implements OnInit {
+export class CoursesComponent {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  courses = [
+    { "id": 1, "name": "Learn Angular", "description": "Start this course to become a full-time Angular developer!", "image": "../../assets/ng.png"},
+    { "id": 2, "name": "Learn TypeScript", "description": "Start this course to become a full-time TypeScript developer!", "image": "../../assets/ts.png"},
+    { "id": 3, "name": "Learn Node.js", "description": "Start this course to become a full-time Node.js developer!", "image": "../../assets/node.jpg"},
+    { "id": 4, "name": "Learn JavaScript", "description": "Start this course to become a full-time JavaScript developer!", "image": "../../assets/js.png"},
+  ]
 
 }
